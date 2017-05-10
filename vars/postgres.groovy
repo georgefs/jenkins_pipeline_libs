ab35af006cbb6ccc2e4154232a65884808f4ff24
georgeli <george.li@gliacloud.com>
2017-05-10 21:18:40 +0800

    update

--- vars/postgres.groovy
+++ vars/postgres.groovy
@@ -5,7 +5,7 @@ def call(body){
     def container = sh(returnStdout: true, script: "docker run -d --rm -p 5432 -e POSTGRES_USER=${user} -e POSTGRES_PASSWORD=${password} postgres").trim()
     def port = sh(returnStdout: true, script: "docker port ${container} 5432|sed 's/0.0.0.0://'").trim()
     def db = "db"
-    withEnv(["$GHOST=${host}", "PGUSER=${user}", "PGPASSWORD=${password}", "PGPORT=${port}", "POSTGRES_DB=${db}"]){
+    withEnv(["PGHOST=${host}", "PGUSER=${user}", "PGPASSWORD=${password}", "PGPORT=${port}", "POSTGRES_DB=${db}"]){
         body()
     }
     sh "docker rm -f ${container}"
