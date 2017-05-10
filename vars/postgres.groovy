6a84daeabdf1fb35638a7db6dbba067acf91e4ac
georgeli <george.li@gliacloud.com>
2017-05-10 20:29:09 +0800

    add postgres plugin

---
+++ vars/postgres.groovy
@@ -0,0 +1,11 @@
+def call(body){
+    def host = "172.17.42.1"
+    def user = "root"
+    def password = "test"
+    def container = sh(returnStdout: true, script: "docker run -d --rm -p 5432 -e POSTGRES_USER=${user} -e POSTGRES_PASSWORD=${password} postgres").trim()
+    def port = sh(returnStdout=true, script: "docker port ${container} | sed 's/0.0.0.0//'").trim()
+    withEnv(["PGHOST=${host}", "PGUSER=${user}", "PGPASSWORD=${password}", "PGPORT=${port}"]){
+        body()
+    }
+    sh "docker rm -f ${container}"
+}
