cb1c5121544f0b47460aafc23b12756e0cf49e80
georgeli <george.li@gliacloud.com>
2017-05-21 03:29:27 +0800

    update

--- vars/aws.groovy
+++ vars/aws.groovy
@@ -4,6 +4,7 @@ def install(){
 
 
 def login(){
+    install()
     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'aws', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
         sh "aws configure set aws_access_key_id ${USERNAME}"
         sh "aws configure set aws_secret_access_key ${PASSWORD}"
