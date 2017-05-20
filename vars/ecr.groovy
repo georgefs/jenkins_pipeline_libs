39401665d8f2e076b9f54c66e7b5ad93f7c7d0c5
georgeli <george.li@gliacloud.com>
2017-05-21 03:37:52 +0800

    update

--- vars/ecr.groovy
+++ vars/ecr.groovy
@@ -18,18 +18,14 @@ def push_image(name, version='latest', origin_name=null){
     origin_name = origin_name || name
     def real_name = "${ECR_DOMAIN}/${name}:${version}"
     sh "docker tag ${origin_name} ${real_name}"
-    docker.withRegistry("https://${ECR_DOMAIN}", "${ECR_KEY}") {
-        sh "docker push ${real_name}" 
-    }
+    sh "docker push ${real_name}"
 }
 
 def pull_image(name, version="latest", target_name=null){
     ecr_login()
     target_name = target_name || name
     def real_name = "${ECR_DOMAIN}/${name}:${version}"
-    docker.withRegistry("https://${ECR_DOMAIN}", "${ECR_KEY}") {
-        sh "docker pull ${real_name}"
-        sh "docker tag ${real_name} ${name}"
-    }
+    sh "docker pull $real_name"
+    sh "docker tag ${real_name} ${name}"
     return name
 }
