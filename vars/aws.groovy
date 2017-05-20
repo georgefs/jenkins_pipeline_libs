c9b71c543298b2a74271e79a858416135a052e95
georgeli <george.li@gliacloud.com>
2017-05-21 03:10:03 +0800

    update

--- vars/aws.groovy
+++ vars/aws.groovy
@@ -1,3 +1,12 @@
+def install(){
+    sh """ 
+	if ! [ -x "$(command -v aws)" ]; then
+		sudo apt-get update && sudo apt-get install awscli
+	fi
+    """
+}
+
+
 def login(){
     withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'aws', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
         sh "aws configure set aws_access_key_id ${USERNAME}"
